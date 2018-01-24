465043cfa16d616f3757e6dbf97a65a6a3073763
hlhr <henning.loehr@gmail.com>
2018-01-24 22:43:26 +1300

    hash password when creating user

--- lib/phoenix_authentication/accounts/user.ex
+++ lib/phoenix_authentication/accounts/user.ex
@@ -20,4 +20,31 @@ defmodule PhoenixAuthentication.Accounts.User do
     |> validate_required([:name, :email])
     |> unique_constraint(:email)
   end
+
+  @doc false
+  def update_changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:name, :email], [:password])
+    |> validate_required([:name, :email])
+    |> put_pass_hash
+    |> unique_constraint(:email)
+  end
+
+  @doc false
+  def registration_changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+    |> put_pass_hash
+    |> unique_constraint(:email)
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
 end
